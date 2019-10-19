76028e5d5d07acd60b55610eeb16d7e94be1c931
FengZChen <912191424@qq.com>
2019-10-19 15:06:52 +0800

    用户选择功能完善

--- src/main/java/org/hackthon/letsdinner/controller/UserMenuController.java
+++ src/main/java/org/hackthon/letsdinner/controller/UserMenuController.java
@@ -1,7 +1,11 @@
 package org.hackthon.letsdinner.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.hackthon.letsdinner.model.food;
+import org.hackthon.letsdinner.model.FoodBean;
+import org.hackthon.letsdinner.model.SelectBean;
+import org.hackthon.letsdinner.utils.PeriodUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +13,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class UserMenuController {
@@ -24,28 +31,72 @@ public class UserMenuController {
     @RequestMapping("/userMenu")
     public String userMenu(HttpServletRequest request, Model model)
     {
+        // 获取用户ID
         String userID = "";
         if (request.getParameterMap().get("userID") != null) {
             userID = request.getParameterMap().get("userID")[0];
         }
-
-        // 获取当前时间
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  //设置日期格式
-        String date = df.format(new Date());  // new Date()为获取当前系统时间，也可使用当前时间戳
-
-        // 根据日期获取菜品信息
-
         //菜品信息初始化
         ArrayList menu = new ArrayList();
-        for(int i = 0; i < 12; i++) {
-            food f = new food();
-            f.foodId = i;
-            f.foodName = "宫保鸡丁" + i;
-            f.price = 8;
-            menu.add(f);
+
+        // 获取当前时间
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");  //设置日期格式
+        Date currentDt = new Date();
+        String date = df.format(currentDt);
+        // 获取当前用餐时间段
+        String mealTime = PeriodUtil.judgeCurrentTime(currentDt);
+        // 获取时间段失败
+        if (mealTime == null)
+        {
+            return "error";
         }
+        // 根据日期和用餐类型获取菜品信息
+        // String foodListStr = MenuDayDao.getMenu(date, mealTime);
+
+        // 调试代码
+        String foodListStr = "";
+        String encoding = "UTF-8";
+        File file = new File("D:\\fsq\\test.txt");
+        Long filelength = file.length();
+        byte[] filecontent = new byte[filelength.intValue()];
+        try {
+            FileInputStream in = new FileInputStream(file);
+            in.read(filecontent);
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            foodListStr = new String(filecontent, encoding);
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("The OS does not support " + encoding);
+            e.printStackTrace();
+            return null;
+        }
+
+        // json解析
+        JSONObject foodListJson = JSONObject.parseObject(foodListStr);
+        Integer statusCode = foodListJson.getInteger("code");
+        // 获取菜单列表成功
+        if (statusCode == 0)
+        {
+            JSONArray foodList = foodListJson.getJSONArray("data");
+            for(int i = 0; i < foodList.size(); i++) {
+                JSONObject foodJson = (JSONObject) foodList.get(i);
+                FoodBean everyFood = JSON.toJavaObject(foodJson, FoodBean.class);
+                menu.add(everyFood);
+            }
+        }
+        else {
+            return "error";
+        }
+
         model.addAttribute("menu", menu);
         model.addAttribute("userID", userID);
+        model.addAttribute("date", date);
+        model.addAttribute("period", mealTime);
         return "userMenu";
     }
 
@@ -55,29 +106,45 @@ public class UserMenuController {
     String userCommit(HttpServletRequest request)
     {
         // 获取用户ID及选择的菜品
-        String sendStr = request.getParameterMap().get("sendJson")[0];
+        String sendStr = request.getParameterMap().get("foodJson")[0];
+        String date = request.getParameterMap().get("date")[0];
+        String period = request.getParameterMap().get("period")[0];
+
         JSONObject sendJson = JSONObject.parseObject(sendStr);
         String userID = sendJson.get("userID").toString();
         String foodStr = sendJson.get("foodJson").toString();
-        foodStr = foodStr.replace("[", "{").replace("]", "}");
 
+        // 存储菜品ID和数量
+        HashMap<Integer, Integer> foodMap = new HashMap<Integer, Integer>();
+        JSONArray foodArray = JSONArray.parseArray(foodStr);
+        for(int i = 0; i < foodArray.size(); i++) {
+            JSONObject foodJson = (JSONObject) foodArray.get(i);
+            SelectBean everyFood = JSON.toJavaObject(foodJson, SelectBean.class);
+            foodMap.put(everyFood.id, everyFood.num);
+        }
         // 加入时间戳，生成唯一ID
         String uniqueID = System.currentTimeMillis() + userID;
 
         //将用户选择的菜单存入数据库
-
+        //String saveMsg = MenuOneDao.addOneMenu(userID, foodMap, uniqueID, date, period);
+        String saveMsg = "{\n" +
+                "    \"code\": 0,\n" +
+                "    \"message\":\"\",\n" +
+                "    \"data\": {\n" +
+                "    }\n" +
+                "  }";
+        JSONObject foodListJson = JSONObject.parseObject(saveMsg);
+        Integer statusCode = foodListJson.getInteger("code");
         JSONObject returnJson = new JSONObject();
-        returnJson.put("uniqueID", uniqueID);
-
+        // 存储用户选菜列表成功
+        if (statusCode == 0)
+        {
+            returnJson.put("code", 0);
+            returnJson.put("uniqueID", uniqueID);
+        }
+        else {
+            returnJson.put("code", -1);
+        }
         return returnJson.toString();
     }
-
-    /*@RequestMapping("/menu")
-    public ModelAndView  itemsList() {
-        String sql = "select * from items";
-        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
-        ModelAndView mav = new ModelAndView("items");
-        mav.addObject("list", list);
-        return mav;
-    }*/
 }
