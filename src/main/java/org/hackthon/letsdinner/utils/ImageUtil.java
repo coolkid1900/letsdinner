8305eee549aec65fc82b7cf5aa81fe0227caa0e2
shikong-xu <912549419@qq.com>
2019-10-19 21:20:19 +0800

    第一版

--- src/main/java/org/hackthon/letsdinner/utils/ImageUtil.java
+++ src/main/java/org/hackthon/letsdinner/utils/ImageUtil.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ImageUtil {
+
     public static String image2Base64(String imgFile)
     {
         //将图片文件转化为字节数组字符串，并对其进行Base64编码处理
@@ -28,5 +29,25 @@ public class ImageUtil {
         BASE64Encoder encoder = new BASE64Encoder();
         return encoder.encode(data);//返回Base64编码过的字节数组字符串
 
+    }
+
+    public static String image2Base64byinputStream(InputStream in)
+    {
+        //将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+        byte[] data = null;
+        //读取图片字节数组
+        try
+        {
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        //对字节数组Base64编码
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(data);//返回Base64编码过的字节数组字符串
     }
 }
